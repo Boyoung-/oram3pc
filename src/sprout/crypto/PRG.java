18dbe6fa455ded27d46ed488e98cb926bf086c5d
cwood <cwood@narya-wlan.parc.xerox.com>
2014-09-07 14:37:57 -0700

    Adding PRG

---
+++ src/sprout/crypto/PRG.java
@@ -0,0 +1,49 @@
+package sprout.crypto;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import sprout.crypto.CryptoException;
+
+import java.math.BigInteger;
+
+public class PRG {
+
+	private SecureRandom rand;
+	private int l;
+
+	public PRG(byte[] seed, int l) throws NoSuchAlgorithmException {
+		this.rand = SecureRandom.getInstance("SHA1PRNG");
+		this.rand.setSeed(seed);
+	}
+
+	public byte[] generateBytes(int m) {
+		byte[] bytes = new byte[(m + 7) / 8];
+		this.rand.nextBytes(bytes);
+		return bytes;
+	}
+
+	public byte[] generateBytes(int m, byte[] seed) {
+		this.rand.setSeed(seed);
+		return generateBytes(m);
+	}
+
+	public byte[] generateBytes(int m, BigInteger seed) {
+		return generateBytes(m, seed.toByteArray());
+	}
+
+	public String generateBitString(int m) {
+		byte[] bytes = generateBytes(m);
+		return new BigInteger(bytes).toString(2).substring(0, m); // bit string of length m
+	}
+
+	public String generateBitString(int m, byte[] seed) {
+		byte[] bytes = generateBytes(m, seed);
+		return new BigInteger(bytes).toString(2).substring(0, m); // bit string of length m
+	}
+
+	public String generateBitString(int m, BigInteger seed) {
+		byte[] bytes = generateBytes(m, seed);
+		return new BigInteger(bytes).toString(2).substring(0, m); // bit string of length m
+	}
+}
