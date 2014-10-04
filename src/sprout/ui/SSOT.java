16da6d75cdac54695f82a946cf3dea74446b3ffa
Boyoung- <byaweics@gmail.com>
2014-10-04 15:42:28 -0700

    addedmore annotations to OTs and PET

--- src/sprout/ui/SSOT.java
+++ src/sprout/ui/SSOT.java
@@ -10,6 +10,7 @@ public class SSOT
 {
 	static SecureRandom rnd = new SecureRandom();
 	
+	// SSOT
 	public static String[][] executeSSOT(String[] sC, String[] sE, Integer[] i) throws NoSuchAlgorithmException {
 		String[][] ret = new String[2][];
 		
@@ -19,22 +20,30 @@ public class SSOT
     	
     	// protocol
 		// step 1
+    	// party I
     	String[] delta = new String[k];
     	for (int o=0; o<k; o++)
     		delta[o] = Util.addZero(new BigInteger(l, rnd).toString(2), l);
     	
     	// step 2
+    	// parties run IOT(E, C, I) on inputs sE for E and i, delta for I
     	String[] a = IOT.executeIOT(sE, i, delta);
-    	ret[1] = a;
+    	// IOT outputs a for C
+    	ret[0] = a;
+    	// C outputs a
     	
     	// step 3
+    	// parties run IOT(C, E, I) on inputs sC for C and i, delta for I
     	String[] b = IOT.executeIOT(sC, i, delta);
-    	ret[0] = b;
+    	// IOT outputs b for E
+    	ret[1] = b;
+    	// E outputs b
     	
     	return ret;
 	}
 		
 	public static void main(String[] args) throws NoSuchAlgorithmException {
+		// for testing
 		String[] sC = new String[]{"000", "001", "010", "011", "100", "101", "110", "111"};
 		String[] sE = new String[]{"000", "000", "000", "000", "000", "000", "000", "000"};
 		Integer[] i = new Integer[]{0, 1, 3, 6};
