599d181e0614ea0948f3c84e7ed6fa75bf324e09
Boyoung- <byaweics@gmail.com>
2014-11-07 15:24:32 -0800

    stopwatch added

--- src/sprout/ui/ForestTest.java
+++ src/sprout/ui/ForestTest.java
@@ -9,6 +9,7 @@ import sprout.oram.Forest;
 import sprout.oram.ForestMetadata;
 import sprout.oram.Tree;
 import sprout.oram.Tuple;
+import sprout.util.StopWatch;
 import sprout.util.Util;
 
 public class ForestTest
@@ -19,75 +20,14 @@ public class ForestTest
 	{
 		ForestMetadata.setup("config/newConfig.yaml");
 		
-		// test tuple
-		/*
-		BigInteger input = new BigInteger(80, rnd).setBit(80);
-		Tuple t = new Tuple(3, Util.rmSignBit(input.toByteArray()));
-		System.out.println(input.toString(2));
-		System.out.println(t);
-		*/
-		
-		// test bucket
-		/*
-		Tuple[] tuples = new Tuple[4];
-		for (int i=0; i<4; i++) {
-			BigInteger input = new BigInteger(80, rnd).setBit(80);
-			tuples[i] = new Tuple(3, Util.rmSignBit(input.toByteArray()));
-			System.out.println(tuples[i]);
-		}
-		byte[] nonce = Util.rmSignBit(new BigInteger(128, rnd).toByteArray());
-		System.out.println(new BigInteger(1, nonce).toString(16));
-		Bucket b = new Bucket(3, nonce, tuples);
-		Tuple[] tuples2 = b.getTuples();
-		for (int i=0; i<4; i++) {
-			System.out.println(new BigInteger(1, tuples2[i].toByteArray()).compareTo(new BigInteger(1, tuples[i].toByteArray())) == 0);
-		}
-		System.out.println(b);
-		
-		byte[] n = b.getNonce();
-		byte[] ts = b.getByteTuples();
-		Bucket b2 = new Bucket(3, n, ts);
-		System.out.println(b2);
-		
-		Bucket b3 = new Bucket(3, b2.toByteArray());
-		System.out.println(b3);
-		
-		int bucketTupleBytes = ForestMetadata.getBucketTupleBytes(3);
-		b3.setTuples(new byte[bucketTupleBytes]);
-		for (int i=0; i<4; i++) {
-			b3.setTuple(tuples[i], i);
-		}
-		System.out.println(b3);
-		
-		for (int i=0; i<4; i++) {
-			Tuple tmp = b3.getTuple(i);
-			System.out.println(new BigInteger(1, tmp.toByteArray()).compareTo(new BigInteger(1, tuples[i].toByteArray())) == 0);
-		}
-		*/
-		
-		// test forest generation
-		long start = System.currentTimeMillis();;
+		StopWatch sw = new StopWatch("Forest Initialization");
+		sw.start();
 		
 		Forest forest = new Forest();
 		//Forest forest = new Forest("files/forest.bin");
 		
-	    long end = System.currentTimeMillis();;
-	    System.out.println("Execution time: " + (end-start)/1000 + "s");
-	    
-	    //forest.printDecryptionToFile("files/test3.txt");
-		
-		// test get/set path
-		/*
-		System.out.println("===== Testing get/set path =====");
-		Tree t = forest.getTree(3);
-		List<Bucket> b1 = t.getBucketsOnPath(1);
-		t.setBucketsOnPath(b1, 3);
-		List<Bucket> b2 = t.getBucketsOnPath(3);
-		Util.printListV(b1);
-		System.out.println();
-		Util.printListV(b2);
-		System.out.println();
-		*/
+		sw.stop();
+		System.out.println(sw);
 	}
 
 }
