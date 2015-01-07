17d62b8e412759413492f15e355d9b4c18e9bcb1
Boyang Wei <byaweics@gmail.com>
2015-01-07 14:56:01 -0800

    small change in some scripts

--- scripts/eddie.sh
+++ scripts/eddie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
-java -cp "${DIR}/bin:${DIR}/lib/*" sprout.ui.TestCLI "$@" eddie
+java -cp "${DIR}/bin:${DIR}/lib/*" sprout.ui.TestCLI -test_alg "$@" eddie
