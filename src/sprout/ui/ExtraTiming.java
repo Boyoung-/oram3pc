5545dc25f0b79b7ad9f73b5aef4e8dfe2b3eac23
Boyoung- <byaweics@gmail.com>
2015-01-29 14:45:14 -0800

    adjusted computation benchmark

--- src/sprout/ui/ExtraTiming.java
+++ src/sprout/ui/ExtraTiming.java
@@ -14,17 +14,17 @@ public class ExtraTiming {
 		StopWatch prg_sw = new StopWatch("PRG");
 		StopWatch pet_sw = new StopWatch("PET");
 
-		int iteration = 2000;
+		int iteration = 20000;
 		int convert = 1000000;
-		int n_start = 200;
+		int n_start = 100;
 		int n_end = 1000;
-		int n_increment = 200;
-		
+		int n_increment = 100;
+
 		byte[] k = new byte[16];
 		BigInteger p2 = SR.p.multiply(SR.p);
 		byte[][] input = new byte[iteration][];
 		byte[][] seed = new byte[iteration][];
-		BigInteger[][] mult = new BigInteger[iteration][];
+		BigInteger[] mult;
 
 		// prepare input
 		for (int i = 0; i < iteration; i++) {
@@ -33,9 +33,8 @@ public class ExtraTiming {
 			SR.rand.nextBytes(input[i]);
 			SR.rand.nextBytes(seed[i]);
 		}
-		
-		System.out.println("\nIterations," + iteration / 2
-				+ ",(below time is sum for each n)");
+
+		System.out.println("\nIterations," + iteration / 2);
 		System.out.println("n,,AES,PRG,PET");
 
 		// start timing
@@ -43,7 +42,8 @@ public class ExtraTiming {
 			SR.rand.nextBytes(k);
 			AES_PRF prf = new AES_PRF(128 * n);
 			prf.init(k);
-			PRG prg = new PRG(128 * n);
+			PRG prg = new PRG(160 * n);
+			mult = new BigInteger[n];
 
 			for (int i = 0; i < iteration; i++) {
 				if (i == iteration / 2)
@@ -58,28 +58,24 @@ public class ExtraTiming {
 				prg.compute(seed[i]);
 			}
 			prg_sw.stop();
-			
+
 			// prepare input
-			for (int i = 0; i < iteration; i++) {
-				//System.out.println(n + "  " + i);
-				mult[i] = new BigInteger[4 * n];
-				for (int j = 0; j < 4 * n; j++) {
-					mult[i][j] = Util.nextBigInteger(p2);
-				}
+			for (int i = 0; i < n; i++) {
+				mult[i] = Util.nextBigInteger(p2);
 			}
 
 			for (int i = 0; i < iteration; i++) {
 				if (i == iteration / 2)
 					pet_sw.start();
-				for (int j = 0; j < 4 * n; j++) {
-					mult[i][j].mod(SR.p);
+				for (int j = 0; j < n; j++) {
+					mult[j].mod(SR.p);
 				}
 			}
 			pet_sw.stop();
 
-			//aes_sw.divide(iteration);
-			//prg_sw.divide(iteration); 
-			//pet_sw.divide(iteration);
+			// aes_sw.divide(iteration);
+			// prg_sw.divide(iteration);
+			// pet_sw.divide(iteration);
 
 			System.out.print(n + ",WC(ms)");
 			System.out.print("," + aes_sw.elapsedWallClockTime / convert);
@@ -95,7 +91,7 @@ public class ExtraTiming {
 			prg_sw.reset();
 			pet_sw.reset();
 		}
-	
+
 	}
 
 }
