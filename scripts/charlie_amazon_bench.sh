bbea27a4037c4b278e3418d21cd244c1a0795474
EC2 Default User <ec2-user@ip-172-31-17-4.us-west-2.compute.internal>
2015-01-30 01:07:38 +0000

    added some benchmark scripts

---
+++ scripts/charlie_amazon_bench.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+java -cp "${DIR}/../bin:${DIR}/../lib/*" sprout.ui.CommunicationBenchmark -eddie_ip 54.69.155.222 -debbie_ip 54.149.252.98 charlie
