38035d46ca51bfa4ada11c69e32eca002e210fee
Boyoung- <byaweics@gmail.com>
2015-02-24 18:04:25 -0800

    updating aws ips

--- scripts/charlie_amazon.sh
+++ scripts/charlie_amazon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
-java -cp "${DIR}/bin:${DIR}/lib/*" sprout.ui.TestCLI -test_alg retrieve -eddie_ip 54.149.7.250 -debbie_ip 54.69.67.81 charlie
+java -cp "${DIR}/bin:${DIR}/lib/*" sprout.ui.TestCLI -test_alg retrieve -eddie_ip 52.11.8.158 -debbie_ip 52.11.32.229 charlie
--- scripts/debbie_amazon.sh
+++ scripts/debbie_amazon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
-java -cp "${DIR}/bin:${DIR}/lib/*" sprout.ui.TestCLI -test_alg retrieve -eddie_ip 54.149.7.250 debbie
+java -cp "${DIR}/bin:${DIR}/lib/*" sprout.ui.TestCLI -test_alg retrieve -eddie_ip 52.11.8.158 debbie
