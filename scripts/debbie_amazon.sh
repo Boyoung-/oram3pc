3bb06fa0e8671ed7726f17822d0bf2c59a351b15
Boyoung- <byaweics@gmail.com>
2015-05-21 20:36:00 -0700

    change aws ips

--- scripts/debbie_amazon.sh
+++ scripts/debbie_amazon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
-java -cp "${DIR}/bin:${DIR}/lib/*" sprout.ui.TestCLI -test_alg "$@" -eddie_ip 52.24.77.104 debbie
+java -cp "${DIR}/bin:${DIR}/lib/*" sprout.ui.TestCLI -test_alg "$@" -eddie_ip 52.25.176.85 debbie
--- scripts/debbie_amazon_bench.sh
+++ scripts/debbie_amazon_bench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" sprout.ui.CommunicationBenchmark -eddie_ip 52.24.77.104 debbie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" sprout.ui.CommunicationBenchmark -eddie_ip 52.25.176.85 debbie
