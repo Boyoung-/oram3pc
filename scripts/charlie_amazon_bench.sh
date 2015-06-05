c4ba1702993597f76b69719c9ba3ac9d2e32553a
Boyoung- <byaweics@gmail.com>
2015-06-05 16:19:17 -0700

    update aws ips

--- scripts/charlie_amazon_bench.sh
+++ scripts/charlie_amazon_bench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" sprout.ui.CommunicationBenchmark -eddie_ip 54.84.164.75 -debbie_ip 52.8.180.138 charlie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" sprout.ui.CommunicationBenchmark -eddie_ip 52.25.11.110 -debbie_ip 52.24.131.212 charlie
