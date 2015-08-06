bca6ca05c9b7bca73869c6f8c1f85f30e835cc5d
Boyoung- <byaweics@gmail.com>
2015-08-06 20:51:38 +0800

    update debbie ip

--- scripts/charlie_amazon_bench.sh
+++ scripts/charlie_amazon_bench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" sprout.ui.CommunicationBenchmark -eddie_ip 52.68.80.95 -debbie_ip 52.76.11.179 charlie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" sprout.ui.CommunicationBenchmark -eddie_ip 52.68.80.95 -debbie_ip 52.69.231.166 charlie
